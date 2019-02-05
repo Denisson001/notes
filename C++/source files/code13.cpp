f3f5ad9c20991889a07be9c60265ea64fb546d03
Denisson <shpakovskij.denis@yandex.ru>
2019-02-05 22:57:56 +0500

    C++ lectures

---
+++ C++/source files/code13.cpp
@@ -0,0 +1,55 @@
+template<class T, class Alloc = std::allocator<T>>
+class vect{
+	T* arr;
+	size_t sz, cap;
+	Alloc alloc;
+	using traits = allocator_traits<Alloc>;
+public:
+	vect(){
+		cap = 1;
+		sz = 0;
+		arr = (T*)(new char[sizeof(T)]);
+	}
+};
+
+void push_back(const T &x){
+	if (sz == cap){
+		T* newarr = traits::allocate(alloc, cap * 2);
+		for (int i = 0; i < cap; ++i){
+			traits::construct(alloc, newarr + i, arr[i]);
+		}
+		for (int i = 0; i < cap; ++i){
+			traits::destroy(alloc, arr + i);
+		}
+		traits::deallocate(alloc, arr, cap);
+		arr = newarr;
+		cap <<= 1;
+	}
+	//arr[sz] = x;
+	//new(arr + sz) T(x);
+	//alloc.construct(arr + sz, x);
+	traits::construct(alloc, arr + sz, x);
+	sz++;
+}
+
+template <class ... Args>
+void emplace_back(const Args &... args) { //... }
+
+T& operator[](size_t i) {
+    return arr[i];
+}    
+
+const T& operator[](size_t i) const { return arr[i]; } //for const vector
+
+traits::select_on_container_copy_construction(alloc);
+
+
+int main(){
+	vector<int> v(100);
+	sizeof(v);
+}
+
+int main(){
+	vector<int> v(100);
+	delete[] &(v[0]);
+}
---
+++ C++/workspace/code12.cpp
@@ -0,0 +1,83 @@
+T* p = new T(...);
+new (p) T(...);
+p->~T();
+
+void* operator new(size_t n){}
+
+void* operator new(size_t n, void *p){ return p; }
+
+new (p, ...) T(...);
+void* operator new(size_t n, ...){}
+
+void* operator new(size_t n, int k){
+	//code here...
+}
+
+int main(){
+	int *p = new(42) int;
+}
+
+class Animal {
+	public:
+	virtual void say() = 0;
+	virtual ~Animal() {}
+};
+
+class Sheep : public Animal {
+	public:
+	virtual void say() {
+		printf("Sheep says baaaaa\n");
+	}
+	
+	virtual ~Sheep() {
+		printf("Sheep is dead\n");
+	}
+	
+	void operator delete(void* p) {
+		printf("Reclaiming Sheep storage from %p\n", p);
+		::operator delete(p);
+	}
+};
+
+int main(int argc, char** argv) {
+	Animal* ap = new Sheep;
+	ap->say();
+	delete ap;
+}
+
+#include <new>
+#include <memory>
+struct nothrow_t {};
+new(std::nothrow) T();
+void* operator new (std::size_t size, const std::nothrow_t &nothrow_value) noexcept;
+
+std::new_handler std::set_new_handler(std::new_handler new_p)
+std::new_handler std::get_new_handler() 
+
+template<class T>
+struct std::allocator<T> {
+	typedef T value_type;
+	T* allocate(size_t n) const {
+		return (T*)(new char[n * sizeof(T)]); 
+	}
+	void deallocate(T* p, size_t n) const {
+		delete[] (char*)p;	
+	}
+	template<typename ... Args>
+	void* construct(T* p, const Args &... args) const {
+		return new(p) T(args...);	
+	} 
+	void destroy(T* p) const {
+		p->~T();
+	}
+};
+
+template<class Alloc>
+std::allocator_traits<Alloc>
+
+template<class Alloc>
+std::allocator_traits<Alloc>{
+	static Alloc::value_type* allocate(Alloc &a, size_t n){
+		return a.allocate();		
+	} 
+}
